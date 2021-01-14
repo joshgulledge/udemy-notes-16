f1d7af1d8a6d4cd20ab77040e9f847eb261aab25
Josh Gulledge <joshcalebgulledge@gmail.com>
2021-01-14 11:03:09 -0600

    coding challenge 3

--- script.js
+++ script.js
@@ -413,6 +413,8 @@ const getJson = function (url, errorMsg = 'something went wrong') {
   });
 };
 
+/*
+
 const get3Countries = async function (c1, c2, c3) {
   try {
     // const [data1] = await getJson(
@@ -439,3 +441,113 @@ const get3Countries = async function (c1, c2, c3) {
 };
 
 get3Countries('usa', 'jamaica', 'greece');
+
+*/
+
+// // Promise.race
+// (async function () {
+//   const res = await Promise.race([
+//     getJson(`https://restcountries.eu/rest/v2/name/italy`),
+//     getJson(`https://restcountries.eu/rest/v2/name/egypt`),
+//     getJson(`https://restcountries.eu/rest/v2/name/mexico`),
+//   ]);
+//   console.log(res[0]);
+// })();
+
+// const timeout = function (sec) {
+//   return new Promise(function (_, reject) {
+//     setTimeout(function () {
+//       reject(new Error('Request took too long!'));
+//     }, sec * 1000);
+//   });
+// };
+
+// Promise.race([
+//   getJson(`https://restcountries.eu/rest/v2/name/honduras`),
+//   timeout(0.01),
+// ])
+//   .then(res => console.log(res[0]))
+//   .catch(err => console.error(err));
+
+// ------------------------------------
+// -------- Coding Challenge 3 --------
+
+/* 
+PART 1
+Write an async function 'loadNPause' that recreates Coding Challenge #2, this time using async/await (only the part where the promise is consumed). Compare the two versions, think about the big differences, and see which one you like more.
+Don't forget to test the error handler, and to set the network speed to 'Fast 3G' in the dev tools Network tab.
+
+PART 2
+1. Create an async function 'loadAll' that receives an array of image paths 'imgArr';
+2. Use .map to loop over the array, to load all the images with the 'createImage' function (call the resulting array 'imgs')
+3. Check out the 'imgs' array in the console! Is it like you expected?
+4. Use a promise combinator function to actually get the images from the array 😉
+5. Add the 'paralell' class to all the images (it has some CSS styles).
+
+TEST DATA: ['img/img-1.jpg', 'img/img-2.jpg', 'img/img-3.jpg']. To test, turn off the 'loadNPause' function.
+
+GOOD LUCK 😀
+*/
+
+const imgContainer = document.querySelector('.images');
+
+const imageGetter = function (imgPath) {
+  return new Promise(function (resolve, reject) {
+    const img = document.createElement('img');
+    img.src = imgPath;
+
+    img.addEventListener('load', function () {
+      imgContainer.append(img);
+      resolve(img);
+    });
+
+    img.addEventListener('error', function () {
+      reject(new Error('Image not found'));
+    });
+  });
+};
+
+const wait = function (seconds) {
+  return new Promise(function (resolve) {
+    setTimeout(resolve, seconds * 1000);
+  });
+};
+
+const loadNPause = async function () {
+  try {
+    // Load image 1
+    let img = await imageGetter('img/img-1.jpg');
+    // let variable because it can change
+    console.log('Image 1 loaded');
+    await wait(2);
+    img.style.display = 'none';
+    // style right away
+
+    // Load image 2
+    img = await imageGetter('img/img-2.jpg');
+    console.log('Image 2 loaded');
+    await wait(2);
+    img.style.display = 'none';
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+// loadNPause();
+
+const loadAll = async function (imgArr) {
+  try {
+    const imgs = imgArr.map(async img => await imageGetter(img));
+    // ^ returns an array of promises
+
+    const imgsEl = await Promise.all(imgs);
+
+    imgsEl.forEach(img => {
+      img.classList.add('parallel');
+    });
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+loadAll(['img/img-1.jpg', 'img/img-2.jpg', 'img/img-3.jpg']);
